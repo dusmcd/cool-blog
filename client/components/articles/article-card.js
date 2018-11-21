6aa0a45c755d200002b866b0ec7e3c04275b43d3
Dustin McDowell <dustinmcdowell1989@gmail.com>
2018-11-20 20:57:52 -0700

    shortening content in landing page

--- client/components/articles/article-card.js
+++ client/components/articles/article-card.js
@@ -2,15 +2,28 @@ import React from 'react'
 import { Segment } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
+const ArticleLink = props => {
+  return (
+    <Link to={`/articles/view/${props.id}?title=${props.slug}`}>
+      {props.children}
+    </Link>
+  )
+}
+
 const ArticleCard = props => {
   const { article } = props
   const cleanContent = article.content.replace(/<br \/>/g, ' ')
   return (
     <Segment raised>
-      <Link to={`/articles/view/${article.id}?title=${article.slug}`}>
+      <ArticleLink id={article.id} slug={article.slug}>
         <h3>{article.title}</h3>
-      </Link>
-      <p>{cleanContent}</p>
+      </ArticleLink>
+      <p>
+        {cleanContent.substring(0, 100)} ...
+        <ArticleLink id={article.id} slug={article.slug}>
+          <span>see more</span>
+        </ArticleLink>
+      </p>
     </Segment>
   )
 }
